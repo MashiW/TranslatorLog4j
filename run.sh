d2023fd0bec3c9024d25730092f082e787ba7849
MashiW <maheshiweerasinghe486@gmail.com>
2016-05-11 17:27:00 +0530

    commit with database modifications

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-$CATALINA_HOME/bin/shutdown.sh
-mvn clean install -DskipTests;
-cp /home/hsenid/Projects/TranslatorLog4j/target/LoginTranslator.war /usr/local/tomcat-8.0.32/webapps;
-$CATALINA_HOME/bin/startup.sh
--- target/LoginTranslator/footer.jsp
+++
@@ -1,5 +0,0 @@
-<br/>
-<center>
-    <h6>Copyright &copy; 2016</h6>
-</center>
-
